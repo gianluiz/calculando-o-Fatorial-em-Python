e3a8110dc5541a920bd8ab6e2fe2eccc392b1f21
Gian Luiz <gianluizoliveira13@gmail.com>
2022-10-10 14:06:02 -0300

    Exercício simples de Fatorial

---
+++ Fatorial.py
@@ -0,0 +1,19 @@
+while True: #tratamento de erros inicial, caso o usuário não digite um número.
+    try:
+        n = int(input("Qual o número de Fatorial você quer ver?"))
+        break
+    except:
+        print("Tem certeza que você digitou um número?Tente novamente, pois houve um erro...")
+
+#abaixo começa o programa principal
+c = n
+multiplicador = 1 #se fosse somador ou subtrator, seria 0, mas em divisão e multiplicação, é 1..Essa variável é fundamental para o cálculo do exercício
+print(f"Fatorial de {n}:")
+while c != 0:
+    if c == 1:
+        print(f"1 = ", end=(""))
+    else:
+        print(f"{c} X ", end=(""))
+    multiplicador = multiplicador * c #oque importa mesmo é o cálculo desta variável e o debaixo, onde o 'c' foi perdendo 1 a cada loop, para o fatorial dar certo..
+    c = c - 1                         #o resto é visual...ou seja, os 'prints' dentro do "while' servem para demonstrar o raciocínio do Fatorial
+print(multiplicador)
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # calculando o Fatorial em Python
- Sem usar a biblioteca Math, ainda nos exercícios mais simples, um da Fatorial
+ - Sem usar a biblioteca Math.
+ - Ainda na demonstração dos exercícios mais simples.
+ - Este é um que calcula Fatorial de um número.
+ - O foco é: O cálculo utilizado e a aplicação da estrutura While de forma extremamente simles e inteligente.
+ - A parte interessante do exercício é mostrar, no 'print' o raciocínio lógico do Fatorial, pois eu poderia ter optado por apenas
+inserir o resultado final. (exemplo: o fatorial de 5 é 120, mas todo o raciocínio é exposto no 'print' de forma que a edição também faz parte do exercício)
+
+
+
